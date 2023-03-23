8c1f278c170fdfb8d37c29d6487cf868326f40c6
Donald Abua <abuadonald@gmail.com>
2023-03-23 20:39:15 +0100

    random_related_post

--- serverless/controllers/related.controller.js
+++ serverless/controllers/related.controller.js
@@ -5,8 +5,10 @@ import generateRandomNumber from "../utils/random";
 export default async function relatedController(category, path) {
 	connectdb();
 	const limit = 4;
-	const count = await postModel.count();
-	const skip = generateRandomNumber(0, count - limit);
+	const count = await postModel
+		.find({ _id: { $ne: path }, categories: category })
+		.count();
+	const skip = generateRandomNumber(0, Math.abs(count - limit));
 
 	const relatedPosts = await postModel
 		.find({ _id: { $ne: path }, categories: category }, { _v: 0 })
