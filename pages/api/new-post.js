7c5266da9ccb150b34d7c1081908822eb6a27bda
Donald Abua <abuadonald@gmail.com>
2023-03-30 11:58:55 +0100

    api_route_update

--- pages/api/new-post.js
+++ pages/api/new-post.js
@@ -11,10 +11,19 @@ export default api.post("/api/new-post", async (req, res) => {
 	const { post } = req.body;
 
 	post.isPublished = true;
+	const modPost = {
+		...post,
+		categories: [post.categories[0]._id],
+		author: post.author._id,
+	};
 
-	const data = await postModel.findOneAndUpdate({ title: post.title }, post, {
-		upsert: true,
-	});
+	const data = await postModel.findOneAndUpdate(
+		{ title: modPost.title },
+		modPost,
+		{
+			upsert: true,
+		},
+	);
 	if (!data) return res.json({ success: false });
 	res.json({ success: true });
 });
--- serverless/controllers/post.controller.js
+++ serverless/controllers/post.controller.js
@@ -1,13 +1,18 @@
 import connectdb from "../db/connect";
 import authorModel from "../models/author.model";
+import categoryModel from "../models/category.model";
 import postModel from "../models/post.model";
 
 export default async function postController() {
 	connectdb();
 
 	await authorModel.count();
+	await categoryModel.count();
 
-	const posts = await postModel.find({}, { _v: 0 }).populate("author");
+	const posts = await postModel
+		.find({}, { _v: 0 })
+		.populate("author")
+		.populate("categories");
 
 	return JSON.stringify(posts);
 }
