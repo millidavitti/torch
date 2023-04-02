75b270a8278c7d9a8f009ea7635f47a2a7623944
Donald Abua <abuadonald@gmail.com>
2023-04-02 19:32:31 +0100

    api_route_update

--- pages/api/category-posts.js
+++ pages/api/category-posts.js
@@ -1,13 +1,13 @@
+import connectdb from "../../serverless/db/connect";
 import authorModel from "../../serverless/models/author.model";
 import categoryModel from "../../serverless/models/category.model";
 import postModel from "../../serverless/models/post.model";
-import connectdb from "../../serverless/db/connect";
 import express from "express";
 import parseQuery from "../../serverless/utils/parseQuery";
 
 const api = express();
 
-export default api.get("/api/tags", async (req, res) => {
+export default api.get("/api/category-posts", async (req, res) => {
 	connectdb();
 
 	const { filters, pag } = parseQuery(req.query);
@@ -16,11 +16,11 @@ export default api.get("/api/tags", async (req, res) => {
 	await categoryModel.count();
 
 	const posts = await postModel
-		.find({ tags: { $elemMatch: { $eq: filters.tagName } } }, { _v: 0 })
+		.find({ categories: { $elemMatch: { $eq: filters.category } } }, { _v: 0 })
+		.sort("-_id")
 		.skip(+pag.from || 0)
-		.limit(+pag.limit || 3)
-		.populate("author")
-		.populate("categories");
+		.limit(+pag.limit || 2)
+		.populate("author");
 
-	res.json(posts);
+	return res.json(posts);
 });
